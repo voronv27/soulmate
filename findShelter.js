ea54327aafad0627a62cb28dc3b3bae3e2c37a48
voronv27 <152320775+voronv27@users.noreply.github.com>
2025-10-21 16:40:02 -0400

    Add search bar to find shelters page
    This is still a work in progress, but you should be able to search for shelters in the area. This works locally but I may need to adjust permissions on the API key to get it to work for the site (but this is unrelated to the code--the code itself is functional)

--- findShelter.js
+++ findShelter.js
@@ -1,8 +1,36 @@
-// Finds the 'map' div element and sets its location
-var map = L.map('map').setView([51.505, -0.09], 13)
+const apiKey = '063ef3ad3bd243e48b9e4639ca2544f4'; // when testing, change this to the API key that allows localhost--contact Vicky if you need this
 
-// Display tilemap (the actual map 'image') using openstreetmap
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// Finds the 'map' div element
+var map = L.map('map').setView([43,-73], 10);
+
+var mapURL = L.Browser.retina
+  ? `https://maps.geoapify.com/v1/tile/{mapStyle}/{z}/{x}/{y}.png?apiKey={apiKey}`
+  : `https://maps.geoapify.com/v1/tile/{mapStyle}/{z}/{x}/{y}@2x.png?apiKey={apiKey}`;
+
+// Get rough user location using IP address and set map view there as a starting point
+// Currently this isn't working--I need to see if I'm missing an import or if I need to add something to the API key
+/*fetch(`https://api.geoapify.com/v1/ipinfo?apiKey=${apiKey}`).then(result => result.json()).then(result => {
+    map.setView([result.location.latitude, result.location.longitude], 8);
+});*/
+
+// Add map tiles layer. Set 20 as the maximal zoom and provide map data attribution.
+L.tileLayer(mapURL, {
+  attribution: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" rel="nofollow" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" rel="nofollow" target="_blank">© OpenStreetMap</a> contributors',
+  apiKey: apiKey,
+  mapStyle: "osm-bright-smooth", // More map styles on https://apidocs.geoapify.com/docs/maps/map-tiles/
+  maxZoom: 20
 }).addTo(map);
+
+// Add Geoapify Address Search control
+// TODO: hide the search bar and auto-search for animal shelters & human societies instead
+const addressSearchControl = L.control.addressSearch(apiKey, {
+  position: 'topleft',
+  resultCallback: (address) => {
+    console.log(address)
+  },
+  suggestionsCallback: (suggestions) => {
+    console.log(suggestions);
+  }
+});
+map.addControl(addressSearchControl);
+L.control.zoom({ position: 'bottomright' }).addTo(map);
